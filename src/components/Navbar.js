a8f2db8c316758d4143f83538abdc2a53ed5aada
jeremytuesley <jeremytuesley@gmail.com>
2021-07-14 02:53:10 +1000

    styled nav bar
    added fonts, hover effects, flex styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,12 +6,16 @@ import '../assets/Nav.scss';
 
 function Navbar() {
   return (
-    <div className="topnav">
+      <div className="topnav">
+        <h1>XLEXTENDS</h1>
+        <div>
       <Link to="/">Home </Link>
       <Link to="/catalog">Product Catalog </Link>
           <Link to="/services">Beauty Services </Link>
           <Link to="/sales">Sales </Link>
           <Link to="/contact">Contact </Link>
+              <a href="www.google.com">carthere</a>
+              </div>
     </div>
   );
 };
