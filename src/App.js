75cd58557e3e454c30be8640fd347a86edc1a5d9
jeremytuesley <jeremytuesley@gmail.com>
2021-07-14 00:53:06 +1000

    setup router
    tested 2 components, working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Home from "./components/Home"
+import Catalog from "./components/Catalog"
+// import Services from "./components/Services"
+// import Sales from "./components/Sales"
+// import Contact from "./components/Contact"
+// import Error from "./components/Error"
+// import Navbar from "./components/Navbar"
+// import Footer from "./components/Footer"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <main>
+            {/* <Navbar /> */}
+            <Switch>
+                <Route path="/" component={Home} exact />
+                <Route path="/catalog" component={Catalog} />
+                {/* <Route component={Error} /> */}
+            </Switch>
+        </main>
+    )
 }
 
 export default App;
