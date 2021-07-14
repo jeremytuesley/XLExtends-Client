53261ce00d7daa53c061782059f1a40f62b4575e
jeremytuesley <jeremytuesley@gmail.com>
2021-07-15 07:20:14 +1000

    refactored canvas code, fixed bug
    was initing with each resize, fixed.
    
    var to let

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,18 +1,16 @@
-/* eslint-disable no-restricted-globals */
-
 const Canvas = () => {
-  var canvas = document.querySelector('canvas');
+  let canvas = document.querySelector('canvas');
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  var c = canvas.getContext('2d');
+  let c = canvas.getContext('2d');
   //   mouse interactive
-  var mouse = {
+  let mouse = {
     x: undefined,
     y: undefined,
   };
-  var colors = ['#6a0dad', '#00CCFF', '#FFD700', '#FF69B4', '#FF0000'];
-  var maxR = 40;
-  var minR = 2;
+  let colors = ['#6a0dad', '#00CCFF', '#FFD700', '#FF69B4', '#FF0000'];
+  let maxR = 40;
+  let minR = 2;
   //   construct circles
   function Circles(x, y, dx, dy, radius) {
     this.x = x;
@@ -31,10 +29,16 @@ const Canvas = () => {
     };
     //  function for moving balls
     this.update = function () {
-      if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+      if (
+        this.x + this.radius > window.innerWidth ||
+        this.x - this.radius < 0
+      ) {
         this.dx = -this.dx;
       }
-      if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      if (
+        this.y + this.radius > window.innerHeight ||
+        this.y - this.radius < 0
+      ) {
         this.dy = -this.dy;
       }
       this.x += this.dx;
@@ -56,13 +60,13 @@ const Canvas = () => {
     };
   }
 
-  var circleArr = [];
+  let circleArr = [];
   // function looping for produce 500 balls
   function init() {
-    for (var i = 0; i < 500; i++) {
-      var radius = Math.random() * 3 + 1;
-      var x = Math.random() * (innerWidth - radius * 2) + radius,
-        y = Math.random() * (innerHeight - radius * 2) + radius,
+    for (let i = 0; i < 500; i++) {
+      let radius = Math.random() * 3 + 1;
+      let x = Math.random() * (window.innerWidth - radius * 2) + radius,
+        y = Math.random() * (window.innerHeight - radius * 2) + radius,
         dx = (Math.random() - 0.5) * 2,
         dy = (Math.random() - 0.5) * 2;
       circleArr.push(new Circles(x, y, dx, dy, radius));
@@ -70,8 +74,8 @@ const Canvas = () => {
   }
   function animate() {
     requestAnimationFrame(animate);
-    c.clearRect(0, 0, innerWidth, innerHeight);
-    for (var k = 0; k < circleArr.length; k++) {
+    c.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    for (let k = 0; k < circleArr.length; k++) {
       circleArr[k].update();
     }
   }
@@ -81,9 +85,8 @@ const Canvas = () => {
     mouse.y = event.y;
   });
   window.addEventListener('resize', function () {
-    canvas.width = window.innerWidth;
+    canvas.width = window.window.innerWidth;
     canvas.height = window.innerHeight;
-    init();
   });
   init();
 };
