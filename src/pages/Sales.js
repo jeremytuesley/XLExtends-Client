132bd3aabd1c5ad6f2db1db7616e87fc87d3a53a
jeremytuesley <jeremytuesley@gmail.com>
2021-07-16 04:54:46 +1000

    created and prepared sales for gql
    Sales implemented and filters working, just waiting on services api. For the moment using a mockapi in place of services

--- src/pages/Sales.js
+++ src/pages/Sales.js
@@ -1,7 +1,24 @@
-import React from "react"
+import React from "react";
+import Catalog from "../components/Catalog";
+import { GET_ALL_PRODUCTS } from "../shared/utils";
+import { useQuery } from "@apollo/client";
+import mockAPI from "../mockapi/mockapi.json";
 
 const Sales = () => {
-  return <h1>Sales</h1>
-}
+  const { loading, error, data } = useQuery(GET_ALL_PRODUCTS);
 
-export default Sales
+  const filteredAPIData = mockAPI.getAllServices
+    .concat(data?.getAllProducts)
+    .filter((item) => item?.salePrice);
+
+  return (
+    <Catalog
+      title="Sale Items"
+      dataResult={filteredAPIData}
+      loading={loading}
+      error={error}
+    />
+  );
+};
+
+export default Sales;
