4e652c2793bc0598fab606e1735af930d8b9b5c6
jeremytuesley <jeremytuesley@gmail.com>
2021-07-17 04:05:11 +1000

    added other legal policy skeletons
    Just adding the files and layout of the other policies

---
+++ src/pages/RefundPolicy.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../assets/policies.scss";
+
+const RefundPolicy = () => {
+  return (
+    <div className="content">
+      <div className="legalPolicies">
+        <div className="policyTitle">Refund Policy</div>
+        <div className="policyDate">18th June 2021 - Subject to Change</div>
+        <div className="policyText"></div>
+        <div className="policySubtitle"></div>
+        <div className="policyText"></div>
+        <div className="policySubtitle"></div>
+        <div className="policyText"></div>
+      </div>
+    </div>
+  );
+};
+
+export default RefundPolicy;
---
+++ src/pages/ShipPolicy.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../assets/policies.scss";
+
+const ShipPolicy = () => {
+  return (
+    <div className="content">
+      <div className="legalPolicies">
+        <div className="policyTitle">Shipping Policy</div>
+        <div className="policyDate">18th June 2021 - Subject to Change</div>
+        <div className="policyText"></div>
+        <div className="policySubtitle"></div>
+        <div className="policyText"></div>
+        <div className="policySubtitle"></div>
+        <div className="policyText"></div>
+      </div>
+    </div>
+  );
+};
+
+export default ShipPolicy;
