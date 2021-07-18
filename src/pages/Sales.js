1f5cd21db0678ee6f861291e555f08c4b0b4dcdf
jeremytuesley <jeremytuesley@gmail.com>
2021-07-19 02:24:33 +1000

    updated sales page to reflect both apis
    No longer a mock api, includes service api and product api

--- src/pages/Sales.js
+++ src/pages/Sales.js
@@ -1,22 +1,31 @@
 import React from "react";
 import Catalog from "../components/Catalog";
-import { GET_ALL_PRODUCTS } from "../shared/utils";
+import { GET_ALL_PRODUCTS, GET_ALL_SERVICES } from "../shared/utils";
 import { useQuery } from "@apollo/client";
-import mockAPI from "../mockapi/mockapi.json";
 
 const Sales = () => {
-  const { loading, error, data } = useQuery(GET_ALL_PRODUCTS);
+  const {
+    loading: productLoading,
+    error: productError,
+    data: productData
+  } = useQuery(GET_ALL_PRODUCTS);
 
-  const filteredAPIData = mockAPI.getAllServices
-    .concat(data?.getAllProducts)
+  const {
+    loading: serviceLoading,
+    error: serviceError,
+    data: serviceData
+  } = useQuery(GET_ALL_SERVICES);
+
+  const filteredAPIData = serviceData?.getAllServices
+    .concat(productData?.getAllProducts)
     .filter((item) => item?.salePrice);
 
   return (
     <Catalog
       title="Sale Items"
       dataResult={filteredAPIData}
-      loading={loading}
-      error={error}
+      loading={productLoading || serviceLoading}
+      error={productError || serviceError}
     />
   );
 };
