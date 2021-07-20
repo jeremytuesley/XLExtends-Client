89324ca3c8081f2db150e2e608707cb3bd605cc2
thankyouthatscool <85291297+thankyouthatscool@users.noreply.github.com>
2021-07-20 10:26:48 +1000

    Product creation.

--- src/pages/Admin/Dashboard/Products/Products.js
+++ src/pages/Admin/Dashboard/Products/Products.js
@@ -1,72 +1,111 @@
 import { useMutation, useQuery } from "@apollo/client";
 import { Button, TextField } from "@material-ui/core";
 import axios from "axios";
-import { useState } from "react";
+import { useFormik } from "formik";
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import * as Yup from "yup";
 
 import { CREATE_NEW_PRODUCT, SIGN_REQUEST } from "../../../../shared/utils/api";
 
 const Products = () => {
   const [files, setFiles] = useState([]);
 
-  const [createNewProduct] = useMutation(CREATE_NEW_PRODUCT);
+  const history = useHistory();
+  const [createNewProduct, { data: createNewProductData }] =
+    useMutation(CREATE_NEW_PRODUCT);
   const { data: signRequestData } = useQuery(SIGN_REQUEST, {
     fetchPolicy: "no-cache"
   });
 
-  const handleUpload = async (e) => {
-    e.preventDefault();
+  const formik = useFormik({
+    initialValues: { name: "", description: "", price: 0, salePrice: 0 },
+    onSubmit: async ({ name, description, price, salePrice }) => {
+      const createForm = (file) => {
+        const formData = new FormData();
+        formData.append("file", file);
+        formData.append("signature", signRequestData.signRequest.signature);
+        formData.append("timestamp", signRequestData.signRequest.timestamp);
+        formData.append("api_key", process.env.REACT_APP_CLOUDINARY_API_KEY);
+        return formData;
+      };
 
-    const createForm = (file) => {
-      const formData = new FormData();
-      formData.append("file", file);
-      formData.append("signature", signRequestData.signRequest.signature);
-      formData.append("timestamp", signRequestData.signRequest.timestamp);
-      formData.append("api_key", process.env.REACT_APP_CLOUDINARY_API_KEY);
-      return formData;
-    };
+      let responses;
 
-    let responses;
-
-    try {
-      responses = await Promise.all(
-        [...files].map((file) =>
-          axios.post(
-            process.env.REACT_APP_CLOUDINARY_API_ENDPOINT,
-            createForm(file)
+      try {
+        responses = await Promise.all(
+          [...files].map((file) =>
+            axios.post(
+              process.env.REACT_APP_CLOUDINARY_API_ENDPOINT,
+              createForm(file)
+            )
           )
-        )
-      );
+        );
 
-      const images = responses.map(({ data: { url } }) => url);
+        const images = responses.map(({ data: { url } }) => url);
 
-      createNewProduct({
-        variables: {
-          createNewProductData: {
-            available: true,
-            description: "New Product Description - 911",
-            images,
-            name: "New Product - 911",
-            options: ["what are those"],
-            price: 9.99,
-            salePrice: 8.65
+        createNewProduct({
+          variables: {
+            createNewProductData: {
+              available: true,
+              description,
+              images,
+              name,
+              options: ["what are those"],
+              price,
+              ...(Boolean(salePrice) && { salePrice })
+            }
           }
-        }
-      });
-    } catch {
-      console.log("this is errors");
+        });
+      } catch {
+        console.log("this is errors");
+      }
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required("Product name is required."),
+      description: Yup.string().required("Product description is required."),
+      price: Yup.number().required("Product price is required."),
+      salePrice: Yup.number()
+    })
+  });
+
+  useEffect(() => {
+    if (createNewProductData) {
+      console.log(createNewProductData.createNewProduct._id);
+      if (createNewProductData.createNewProduct._id) {
+        history.push("/products");
+      }
     }
-  };
+  }, [createNewProductData]);
 
   return (
     <div>
       <h1>Products</h1>
       <div>
         <h2>Create New Product</h2>
-        <form onSubmit={handleUpload}>
-          <TextField label="name" variant="outlined" />
-          <TextField label="description" variant="outlined" />
-          <TextField label="price" type="number" variant="outlined" />
-          <TextField label="sale price" type="number" variant="outlined" />
+        <form onSubmit={formik.handleSubmit}>
+          <TextField
+            label="name"
+            variant="outlined"
+            {...formik.getFieldProps("name")}
+          />
+          <TextField
+            label="description"
+            variant="outlined"
+            {...formik.getFieldProps("description")}
+          />
+          <TextField
+            label="price"
+            type="number"
+            variant="outlined"
+            {...formik.getFieldProps("price")}
+          />
+          <TextField
+            label="sale price"
+            type="number"
+            variant="outlined"
+            {...formik.getFieldProps("salePrice")}
+          />
           <Button
             color="secondary"
             component="label"
