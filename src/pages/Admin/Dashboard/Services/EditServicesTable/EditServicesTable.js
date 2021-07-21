53a0e457bb19a7436781e8d13af91743e74a2c78
thankyouthatscool <85291297+thankyouthatscool@users.noreply.github.com>
2021-07-21 15:37:02 +1000

    Services fix.

--- src/pages/Admin/Dashboard/Services/EditServicesTable/EditServicesTable.js
+++ src/pages/Admin/Dashboard/Services/EditServicesTable/EditServicesTable.js
@@ -1,5 +1,5 @@
 import { useQuery } from "@apollo/client";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useTable } from "react-table";
 
 import { EditServiceForm } from "../EditServiceForm";
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,18 +1,12 @@
 import { useQuery } from "@apollo/client";
 
 import Catalog from "../components/Catalog";
-import { CHECK_AVAILABILITY, GET_ALL_SERVICES } from "../shared/utils";
+import { GET_ALL_SERVICES } from "../shared/utils";
 
 const Services = () => {
-  const {
-    data: availabilityData,
-    error: availabilityError,
-    loading: availabilityLoading
-  } = useQuery(CHECK_AVAILABILITY, {
-    fetchPolicy: "no-cache",
-    variables: { checkAvailabilityData: { quantity: 1, timeUnit: "week" } }
+  const { loading, error, data } = useQuery(GET_ALL_SERVICES, {
+    fetchPolicy: "no-cache"
   });
-  const { loading, error, data } = useQuery(GET_ALL_SERVICES);
 
   return (
     <Catalog
